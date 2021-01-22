b5d7e6464b1f84e716e3f8f2d40a321f6fcf567e
Wes Roach <wesley.roach@gdit.com>
2021-01-22 14:07:31 -0500

    ++classifiers; pytest test requirement.

--- setup.py
+++ setup.py
@@ -8,4 +8,13 @@ setup(
     license="MIT",
     packages=["pysoma"],
     install_requires=["pyspark>=3.0.0"],
+    tests_require=["pytest"],
+    classifiers=[
+        "Development Status:: 2 - Pre-Alpha",
+        "Intended Audience :: Healthcare Industry",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+    ],
 )
