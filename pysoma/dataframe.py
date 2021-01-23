cdffb5dbac018bd78546502ed2fc85df1d754194
Wes Roach <wesley.roach@gdit.com>
2021-01-22 22:07:48 -0500

    Add general DataFrame QOL funcs and tests.

--- pysoma/dataframe.py
+++ pysoma/dataframe.py
@@ -1,9 +1,290 @@
+from typing import Union, List
+import types
+from pathlib import Path
+from itertools import chain
+import functools
+import re
 from collections import namedtuple
 
 import pyspark.sql.functions as F
-from pyspark.sql.types import StructType
+from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.types import (
+    ArrayType,
+    BinaryType,
+    BooleanType,
+    DateType,
+    MapType,
+    NullType,
+    NumericType,
+    StringType,
+    StructType,
+    TimestampType,
+    UserDefinedType,
+)
 
 
 def namedtuple_from_schema(schema: StructType, name: str):
     """Convert a PySpark schema into a named tuple."""
     return namedtuple(name, schema.names, defaults=(None,) * len(schema.names))
+
+
+def cols_to_array(
+    df, array_name: str, columns: Union[str, List[str]], remove_na: bool = True
+):
+    """
+    Returns an array named `array_name` containing the values from provided `columns`.
+
+    Args:
+        df (pyspark.sql.DataFrame)
+    """
+    array_column = F.array([F.col(x) for x in columns])
+
+    if remove_na:
+        return df.withColumn(
+            array_name, F.array_except(array_column, F.array(F.lit(None)))
+        )
+    else:
+        return df.withColumn(array_name, array_column)
+
+
+def two_columns_to_dictionary(df, key_col_name, value_col_name):
+    """
+    Creates dict from pair of columns.
+    """
+    k, v = key_col_name, value_col_name
+    return {x[k]: x[v] for x in df.select(k, v).collect()}
+
+
+def dict_to_map_literal(mapping: dict):
+    """
+    Create Spark map literal from dict of k-v pairs.
+    """
+    # create_map requires a list of values
+    # [1, 2, 3, 4] will map 1=>2 and 3=>4
+    # ["A", "a", "B", "b"] will map "A"->"a" and "B"->"b"
+    # ["A", ["ah", "bah"]] will map "A"->array("ah", "bah")
+    # All literal values need to be wrapped with F.lit(literal-value)
+    literal_list = []
+    for k, v in mapping.items():
+        literal_list.append(F.lit(k))
+        if isinstance(v, (str, int, float)):
+            literal_list.append(F.lit(v))
+        elif isinstance(v, list):
+            literal_list.append(F.array([F.lit(x) for x in v]))
+    return F.create_map(literal_list)
+
+
+def map_column(column, mapping_expression):
+    """
+    Apply mapping expression to column.
+    """
+    return mapping_expression[F.col(column)]
+
+
+class DataFrameMissingColumnError(ValueError):
+    """raise this when there's a DataFrame column error"""
+
+
+class DataFrameMissingStructFieldError(ValueError):
+    """raise this when there's a DataFrame column error"""
+
+
+class DataFrameProhibitedColumnError(ValueError):
+    """raise this when a DataFrame includes prohibited columns"""
+
+
+def validate_presence_of_columns(df, required_col_names):
+    """
+    Validate DataFrame contains listed columns.
+    """
+    all_col_names = df.columns
+    missing_col_names = [x for x in required_col_names if x not in all_col_names]
+    error_message = (
+        f"The {missing_col_names} columns are not included in the DataFrame "
+        f"with the following columns {all_col_names}"
+    )
+    if missing_col_names:
+        raise DataFrameMissingColumnError(error_message)
+
+
+def validate_schema(df, required_schema):
+    """
+    Raises Error if df schema does not match required schema.
+
+    Args:
+        df: pyspark.sql.DataFrame
+        required_schema: [pyspark.sql.types.StructField, ...]
+
+    Example:
+        data = [("jose", 1), ("li", 2), ("luisa", 3)]
+        source_df = spark.createDataFrame(data, ["name", "age"])
+        required_schema = StructType(
+            [
+                StructField("name", StringType(), True),
+                StructField("city", StringType(), True),
+            ]
+        )
+        # Raises Error, as "city" does not exist, but "age" does.
+        validate_schema(source_df, required_schema)
+    """
+    all_struct_fields = df.schema
+    missing_struct_fields = [x for x in required_schema if x not in all_struct_fields]
+    error_message = (
+        f"The {missing_struct_fields} StructFields are not included in the "
+        f"DataFrame with the following StructFields {all_struct_fields}"
+    )
+    if missing_struct_fields:
+        raise DataFrameMissingStructFieldError(error_message)
+
+
+def validate_absence_of_columns(df, prohibited_col_names: list):
+    """
+    Validate DataFrame does not contain listed columns.
+    """
+    all_col_names = df.columns
+    extra_col_names = [x for x in all_col_names if x in prohibited_col_names]
+    error_message = (
+        f"The {extra_col_names} columns are not allowed to be included in the "
+        f"DataFrame with the following columns {all_col_names}"
+    )
+    if extra_col_names:
+        raise DataFrameProhibitedColumnError(error_message)
+
+
+def string_to_column_name(x: str):
+    """
+    Converts non-alphanumeric characters to "_" and lowercases string.
+
+    Repeated non-alphanumeric characters are replaced with a single "_".
+    """
+    return re.sub("[^0-9a-zA-Z]+", "_", x).lower()
+
+
+def stage_dataframe_to_disk(
+    cached_file_path: Union[str, Path], overwrite: bool = False
+):
+    """
+    Stage returned DataFrame to disk. Decorated function must return a Spark DataFrame.
+
+    Args:
+        cached_file_path: path to desired parquet location on disk.
+        overwrite: overwrite existing parquet if exists.
+
+    Example
+    -------
+    ```
+    @stage_dataframe_to_disk("path-to-file/file.parquet")
+    def very_important_sdf():
+        data = [(1,), (2,)]
+        sdf = spark.createDataFrame(data, ["col"])
+        return sdf
+
+    # DataFrame is written to disk @ path-to-file/file.parquet
+    very_important_sdf()
+    ```
+    """
+
+    def decorator_to_disk(func):
+        @functools.wraps(func)
+        def wrapper_to_disk(
+            *args, cached_file_path=cached_file_path, overwrite=overwrite, **kwargs
+        ):
+            cached_file_path = Path(cached_file_path)
+            spark = SparkSession.getActiveSession()
+            cached_file_path = Path(cached_file_path)
+            if cached_file_path.exists() is False or overwrite is True:
+                func(*args, **kwargs).write.parquet(
+                    str(cached_file_path), mode="overwrite"
+                )
+            return spark.read.parquet(str(cached_file_path))
+
+        return wrapper_to_disk
+
+    return decorator_to_disk
+
+
+def set_column_order(
+    df: F.DataFrame,
+    column_order: list,
+    remove_unlisted: bool = False,
+):
+    """
+    Set the column order for a DataFrame to a desired order.
+    DataFrame colums not in column_order will be placed at the end of the DataFrame.
+
+    Args:
+        df: DataFrame to reorder columns.
+        column_order: List of column names in desired order.
+
+    Optional Args:
+        remove_unlisted: Remove columns from DataFrame if not in supplied column list.
+            Default: False.
+
+    Returns:
+        DataFrame: DataFrame with columns in desired order.
+    """
+
+    # Type check the inputs
+    if not isinstance(df, (F.DataFrame)):
+        raise TypeError("df must be a Spark DataFrame.")
+    if not isinstance(column_order, list):
+        raise TypeError("column_order must be a list" f"{column_order} was supplied")
+    if not isinstance(remove_unlisted, bool):
+        raise TypeError("remove_unlisted must be a bool")
+
+    # List of columns in both df & column_order
+    ordered_columns_intersect = [col for col in column_order if col in set(df.columns)]
+
+    # columns in df that were not provided to function
+    in_df_not_provided = [col for col in df.columns if col not in set(column_order)]
+
+    if remove_unlisted is False:
+        ordered_columns = ordered_columns_intersect + in_df_not_provided
+    elif remove_unlisted is True:
+        ordered_columns = ordered_columns_intersect
+
+    return df.select(ordered_columns)
+
+
+def rename_by_dict(sdf, renames: dict):
+    """Renames DataFrame columns if they exist as keys in `renames`"""
+
+    # TODO(WES): Using .select([loop-through-select-or-rename]) will save some CPU
+
+    cols_to_rename = [x for x in sdf.columns if x in renames.keys()]
+    for col in cols_to_rename:
+        sdf = sdf.withColumnRenamed(col, renames[col])
+    return sdf
+
+
+def outer_union_corr(*dfs: DataFrame) -> DataFrame:
+    """
+    Returns a new DataFrame containing the union of all rows and columns from passed
+    DataFrames. This is equivalent to OUTER UNION CORR in SQL. To do a SQL-style set
+    union (that does deduplication of elements), use this function followed by
+    distinct(). The difference between this function and unionByName() is that this
+    function returns all columns from all DataFrames.
+
+    Args:
+        dfs (DataFrame): Sequence of DataFrames to union
+
+    Returns:
+        DataFrame
+    """
+    # Identify all unique column names from all dfs
+    all_cols = set.union(*[set(i.columns) for i in dfs])
+
+    for i, df in enumerate(dfs):
+        # Identify missing columns from each df
+        missing_cols = all_cols.difference(set(df.columns))
+
+        # Assign missing columns to each df with null values
+        __df = df.select("*", *[F.lit(None).alias(col) for col in missing_cols])
+
+        # For first df, reassign to _df. Otherwise overwrite and union with _df
+        if i == 0:
+            _df = __df
+        else:
+            _df = _df.unionByName(__df)
+
+    return _df
---
+++ pysoma/tests/test_dataframe.py
@@ -0,0 +1,283 @@
+from pathlib import Path
+
+import pytest
+import pyspark.sql.functions as F
+from pyspark.sql.types import (
+    StructField,
+    ArrayType,
+    BinaryType,
+    BooleanType,
+    ByteType,
+    DecimalType,
+    DoubleType,
+    FloatType,
+    FractionalType,
+    IntegerType,
+    IntegralType,
+    LongType,
+    MapType,
+    NullType,
+    NumericType,
+    StringType,
+    StructType,
+    TimestampType,
+    UserDefinedType,
+    ShortType,
+)
+
+from pysoma.conftest import ensure_clean_dir
+from pysoma.dataframe import (
+    cols_to_array,
+    two_columns_to_dictionary,
+    dict_to_map_literal,
+    DataFrameMissingColumnError,
+    DataFrameMissingStructFieldError,
+    DataFrameProhibitedColumnError,
+    validate_presence_of_columns,
+    validate_schema,
+    validate_absence_of_columns,
+    string_to_column_name,
+    stage_dataframe_to_disk,
+    set_column_order,
+    rename_by_dict,
+    outer_union_corr,
+)
+
+
+def test_cols_to_array(spark_context):
+    sdf = spark_context.createDataFrame(
+        [("001", "11", "12", ["11", "12"]), ("002", "11", None, ["11"])],
+        schema=["id", "col1", "col2", "array_col"],
+    )
+    res = cols_to_array(
+        sdf.drop("array_col"), array_name="array_col", columns=["col1", "col2"]
+    )
+
+    assert sorted(sdf.collect()) == sorted(res.collect())
+
+
+def test_two_columns_to_dictionary():
+    def it_returns_a_dictionary(spark_context):
+        data = [("jose", 1), ("li", 2), ("luisa", 3)]
+        source_df = spark_context.createDataFrame(data, ["name", "age"])
+        actual = two_columns_to_dictionary(source_df, "name", "age")
+        assert {"jose": 1, "li": 2, "luisa": 3} == actual
+
+
+class TestDictToMapLiteral:
+    def test_dict_to_map_literal_basic(self, spark_context):
+        sdf = spark_context.createDataFrame(
+            [("1", "A"), ("2", "B")], schema=["id", "val"]
+        )
+
+        mapping_basic = {"A": "a", "B": "b"}
+        map_literal = dict_to_map_literal(mapping_basic)
+
+        res = sdf.withColumn("mapped", map_literal[F.col("val")])
+
+        exp = spark_context.createDataFrame(
+            [("1", "A", "a"), ("2", "B", "b")], schema=["id", "val", "mapped"]
+        )
+
+        assert sorted(res.collect()) == sorted(exp.collect())
+
+    def test_dict_to_map_literal_nested(self, spark_context):
+        sdf = spark_context.createDataFrame(
+            [("1", "A"), ("2", "B")], schema=["id", "val"]
+        )
+
+        mapping_basic = {"A": ["a", "ah"], "B": ["b", "bah"]}
+        map_literal = dict_to_map_literal(mapping_basic)
+
+        res = sdf.withColumn("mapped", map_literal[F.col("val")])
+
+        exp = spark_context.createDataFrame(
+            [("1", "A", ["a", "ah"]), ("2", "B", ["b", "bah"])],
+            schema=StructType(
+                [
+                    StructField("id", StringType(), False),
+                    StructField("val", StringType(), False),
+                    StructField("mapped", ArrayType(StringType(), False), False),
+                ]
+            ),
+        )
+
+        assert sorted(res.collect()) == sorted(exp.collect())
+
+
+class TestValidatePresenceOfColumns:
+    def test_it_raises_if_a_required_column_is_missing(self, spark_context):
+        data = [("jose", 1), ("li", 2), ("luisa", 3)]
+        source_df = spark_context.createDataFrame(data, ["name", "age"])
+        with pytest.raises(DataFrameMissingColumnError) as excinfo:
+            validate_presence_of_columns(source_df, ["name", "age", "fun"])
+        assert excinfo.value.args[0] == (
+            "The ['fun'] columns are not included in the DataFrame with the following "
+            "columns ['name', 'age']"
+        )
+
+    def test_it_does_nothing_if_all_required_columns_are_present(self, spark_context):
+        data = [("jose", 1), ("li", 2), ("luisa", 3)]
+        source_df = spark_context.createDataFrame(data, ["name", "age"])
+        validate_presence_of_columns(source_df, ["name"])
+
+
+class TestValidateSchema:
+    def test_it_raises_when_struct_field_is_missing1(self, spark_context):
+        data = [("jose", 1), ("li", 2), ("luisa", 3)]
+        source_df = spark_context.createDataFrame(data, ["name", "age"])
+        required_schema = StructType(
+            [
+                StructField("name", StringType(), True),
+                StructField("city", StringType(), True),
+            ]
+        )
+        with pytest.raises(DataFrameMissingStructFieldError) as excinfo:
+            validate_schema(source_df, required_schema)
+        assert excinfo.value.args[0] == (
+            "The [StructField(city,StringType,true)] StructFields are not included "
+            "in the DataFrame with the following StructFields "
+            "StructType(List(StructField(name,StringType,true),"
+            "StructField(age,LongType,true)))"
+        )
+
+    def test_it_does_nothing_when_the_schema_matches(self, spark_context):
+        data = [("jose", 1), ("li", 2), ("luisa", 3)]
+        source_df = spark_context.createDataFrame(data, ["name", "age"])
+        required_schema = StructType(
+            [
+                StructField("name", StringType(), True),
+                StructField("age", LongType(), True),
+            ]
+        )
+        validate_schema(source_df, required_schema)
+
+
+class TestValidateAbsenseOfColumns:
+    def test_it_raises_when_a_unallowed_column_is_present(self, spark_context):
+        data = [("jose", 1), ("li", 2), ("luisa", 3)]
+        source_df = spark_context.createDataFrame(data, ["name", "age"])
+        with pytest.raises(DataFrameProhibitedColumnError) as excinfo:
+            validate_absence_of_columns(source_df, ["age", "cool"])
+        assert excinfo.value.args[0] == (
+            "The ['age'] columns are not allowed to be included in the DataFrame "
+            "with the following columns ['name', 'age']"
+        )
+
+    def test_it_does_nothing_when_no_unallowed_columns_are_present(self, spark_context):
+        data = [("jose", 1), ("li", 2), ("luisa", 3)]
+        source_df = spark_context.createDataFrame(data, ["name", "age"])
+        validate_absence_of_columns(source_df, ["favorite_color"])
+
+
+def test_string_to_column_name():
+    start_end_tuple = [
+        (
+            "Adverse Effect of Beta-Adrenoreceptor Antagonists",
+            "adverse_effect_of_beta_adrenoreceptor_antagonists",
+        ),
+        (
+            "Rotavirus Vaccine (2 Dose Schedule) Administered",
+            "rotavirus_vaccine_2_dose_schedule_administered",
+        ),
+        (
+            "Partial Hospitalization/Intensive Outpatient",
+            "partial_hospitalization_intensive_outpatient",
+        ),
+        (
+            "HbA1c Level 7.0-9.0",
+            "hba1c_level_7_0_9_0",
+        ),
+    ]
+
+    expected = [x[1] for x in start_end_tuple]
+    result = [string_to_column_name(x[0]) for x in start_end_tuple]
+
+    assert sorted(expected) == sorted(result)
+
+
+def test_stage_dataframe_to_disk_function(spark_context):
+
+    with ensure_clean_dir() as tmpdir:
+
+        tmpdir = Path(tmpdir)
+        tmp_parquet_path = tmpdir / "tmp.parquet"
+
+        @stage_dataframe_to_disk(tmp_parquet_path)
+        def tmp_sdf():
+            data = [(1,), (2,)]
+            sdf = spark_context.createDataFrame(data, ["col"])
+            return sdf
+
+        tmp_sdf()
+        tmp_sdf(overwrite=True)
+
+        assert tmp_parquet_path.exists()
+        assert isinstance(
+            spark_context.read.parquet(str(tmp_parquet_path)), F.DataFrame
+        )
+
+
+def test_stage_dataframe_to_disk_method(spark_context):
+
+    with ensure_clean_dir() as tmpdir:
+
+        tmpdir = Path(tmpdir)
+        tmp_parquet_path = tmpdir / "tmp.parquet"
+
+        class TestProcess:
+            @stage_dataframe_to_disk(tmp_parquet_path)
+            def tmp_sdf(self):
+                data = [(1,), (2,)]
+                sdf = spark_context.createDataFrame(data, ["col"])
+                return sdf
+
+        TestProcess().tmp_sdf()
+        TestProcess().tmp_sdf(overwrite=True)
+
+        assert tmp_parquet_path.exists()
+        assert isinstance(
+            spark_context.read.parquet(str(tmp_parquet_path)), F.DataFrame
+        )
+
+
+def test_set_column_order(spark_context):
+    data = [
+        (1, 2, 3),
+    ]
+    schema = ["c1", "c2", "c3"]
+    sdf = spark_context.createDataFrame(data, schema)
+
+    # full set of columns
+    _ = set_column_order(sdf, ["c3", "c2", "c1"])
+    assert _.columns == ["c3", "c2", "c1"]
+
+    # subset of columns
+    _ = set_column_order(sdf, ["c2", "c1"])
+    assert _.columns == ["c2", "c1", "c3"]
+
+    # remove_unlisted = True
+    _ = set_column_order(sdf, ["c3", "c2"], remove_unlisted=True)
+    assert _.columns == ["c3", "c2"]
+
+
+def test_rename_by_dict(spark_context):
+    sdf = spark_context.createDataFrame([(1, 2, 3)], ["col1", "col2", "col3"])
+
+    sdf = rename_by_dict(sdf, {"col1": "_col1", "col4": "_col4"})
+
+    assert sdf.columns == ["_col1", "col2", "col3"]
+
+
+def test_outer_union_corr(spark_context):
+    sdf1 = spark_context.createDataFrame([(1, 2, 3)], ["col1", "col2", "col3"])
+    sdf2 = spark_context.createDataFrame([(4, 5, 6)], ["col1", "col4", "col5"])
+
+    expected_output = spark_context.createDataFrame(
+        [(1, 2, 3, None, None), (4, None, None, 5, 6)],
+        ["col1", "col2", "col3", "col4", "col5"],
+    )
+
+    res = outer_union_corr(sdf1, sdf2).select("col1", "col2", "col3", "col4", "col5")
+
+    assert sorted(res.collect()) == sorted(expected_output.collect())
